5874533059165e916090606404e645681dd2d8e4
Practicante 1 <practicante.uno@mdpp.corp>
2023-11-02 12:05:40 -0500

    Avanzanmos con la parte de admin

--- src/layouts/LayoutAdmin.jsx
+++ src/layouts/LayoutAdmin.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import { SideBar } from '../components/SideBar'
+import { Header } from '../components/Header'
 
 export function LayoutAdmin () {
   return (
-    <div>
-      LayoutAdmin
-      <Outlet />
+    <div className='h-screen grid grid-cols-1 xl:grid-cols-6'>
+      <SideBar />
+      <div className='xl:col-span-5'>
+        <Header />
+        <div className='h-[90vh] overflow-y-auto p-8'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,5 +1,7 @@
 export function Home () {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
